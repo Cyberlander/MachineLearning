0c94e4f60d8b05352f6562af79f42fcbbf3726d5
theSoenke <theSoenke@users.noreply.github.com>
2017-04-30 20:27:05 +0200

    Combine versions

--- 2017/exercise03/gaussian_discriminant_analysis.py
+++ 2017/exercise03/gaussian_discriminant_analysis.py
@@ -4,50 +4,41 @@ from scipy import misc
 
 
 # prints an array of strings
-def read_images( path ):
-    files = glob.glob( path + "/*.png")
+def read_images(path):
+    files = glob.glob(path + "/*.png")
     images = []
     for f in files:
-        images.append( misc.imread( f ) )
+        images.append(misc.imread(f))
     return images
 
 
-negatives = read_images( "negatives" )
-positives = read_images( "positives" )
-
 def min_color_component_image(image):
+    width = len(image)
+    height = len(image[0])
     min_colors = [255, 255, 255]
-    for i in range(0,24):
-        for j in range(0,24):
-            value_red = image[i][j][0]
-            value_green = image[i][j][1]
-            value_blue = image[i][j][2]
-            if value_red < min_colors[0]:
-                min_colors[0] = value_red
-            if value_green < min_colors[1]:
-                min_colors[1] = value_green
-            if value_blue < min_colors[2]:
-                min_colors[2] = value_blue
+
+    for y in range(0, height):
+        for x in range(0, width):
+            min_colors = np.minimum(min_colors, image[y][x])
+
     return min_colors
 
-def min_color_images( images ):
+
+def min_color_images(images):
     min_colors = []
     for image in images:
-        min_colors.append( min_color_component_image( image ))
+        min_colors.append(min_color_component_image(image))
     return min_colors
 
-def min_green_color( images ):
-    min_green = []
-    for image in images:
-        min_green.append( min_color_component_image( image )[1])
-    return min_green
+def feature_matrix():
+    positives = read_images('images/positives')
+    negatives = read_images('images/negatives')
 
-def min_read_color( images ):
-    min_red = []
-    for image in images:
-        min_red.append( min_color_component_image( image )[0])
-    return min_red
+    min_colors_positive = np.array(min_color_images(positives))
+    min_colors_negative = np.array(min_color_images(negatives))
 
+    red, green, blue = 0, 1, 2
+    min_colors_positive_red = min_colors_positive[:,red]
+    print(min_colors_positive_red)
 
-print( min_read_color( negatives ) )
-print( min_read_color( positives ) )
+feature_matrix()
