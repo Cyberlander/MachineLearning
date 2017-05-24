c6c810b210702f0ae4ac01bde2fad7543a8e0384
Cyberlander <felix.froehlich1@gmx.net>
2017-05-24 09:58:58 +0200

    initial commit adaboost

---
+++ 2017/exercise05/ada_boost.py
@@ -0,0 +1,37 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = []
+
+with open( "dataCircle.txt","r" ) as csvfile:
+    csvreader = csv.reader( csvfile, delimiter=" " )
+    for row in csvreader:
+        data = data + row
+
+
+
+
+def data_cleaning( data ):
+    arraysize = len(data)
+    newData = []
+    iterations = 0
+    for i in data:
+        iterations = iterations + 1
+        if i!='':
+            newData.append(i)
+    return newData
+
+def transform_data_to_matrix(data):
+    data = np.array(data,dtype=np.float64)
+    data = np.reshape(data,(-1,3))
+    return data
+
+def initialize_point_importance( points ):
+    count_points = len( points)
+    
+
+data_circle = transform_data_to_matrix( data_cleaning( data ) )
+points = data_circle[:,:-1]
+print( initialize_point_importance(points ))
