12764432989cfa3df82aee0237e550c724efa579
Sönke Behrendt <thesoenke@outlook.com>
2017-07-05 10:36:38 +0200

    Separate data generationa and plotting

--- 2017/exercise06/gaussian_mixtures.py
+++ 2017/exercise06/gaussian_mixtures.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 
 params = [
     ([0, 0], [[1, 0], [0, 100]], 'go'),
@@ -7,8 +8,26 @@ params = [
     ([0, 0], [[100, -20], [0, 10]], "ro")
 ]
 
-for mean, cov, point in params:
-    x, y = np.random.multivariate_normal(mean, cov, 100).T
-    plt.plot(x, y, point)
 
-plt.show()
+def generate_data(params):
+    data = []
+
+    for mean, cov, color in params:
+        x, y = np.random.multivariate_normal(mean, cov, 100).T
+        data.append([x,y])
+
+    return data
+
+def show_plot(data):
+    colors = ['ro','bo','go']
+
+    for i in range(len(data)):
+        x, y = data[i][0], data[i][1]
+        plt.plot(x, y, colors[i])
+
+    plt.show()
+
+
+
+data = generate_data(params)
+show_plot(data)
