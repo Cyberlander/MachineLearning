c38eeb528b5ef6f0aa66552b4ae532c30176debb
Sönke Behrendt <thesoenke@outlook.com>
2017-07-05 10:42:32 +0200

    Generate point class probability

--- 2017/exercise06/gaussian_mixtures.py
+++ 2017/exercise06/gaussian_mixtures.py
@@ -3,20 +3,27 @@ import numpy as np
 from scipy.stats import norm
 
 params = [
-    ([0, 0], [[1, 0], [0, 100]], 'go'),
-    ([0, 0], [[100, 80], [0, 3]], 'bo'),
-    ([0, 0], [[100, -20], [0, 10]], "ro")
+    ([0, 0], [[1, 0], [0, 100]]),
+    ([0, 0], [[100, 80], [0, 3]]),
+    ([0, 0], [[100, -20], [0, 10]])
 ]
+
+guess = {
+    'mean': [1,1]
+    'cov': [[1,0],[0,1]],
+    'class': [0.3, 0.7]
+}
 
 
 def generate_data(params):
     data = []
 
-    for mean, cov, color in params:
+    for mean, covin params:
         x, y = np.random.multivariate_normal(mean, cov, 100).T
         data.append([x,y])
 
     return data
+
 
 def show_plot(data):
     colors = ['ro','bo','go']
@@ -27,6 +34,13 @@ def show_plot(data):
 
     plt.show()
 
+
+def probability(values, mean, cov, p):
+    prob = p
+    for i in range(len(values)):
+        prob *= norm.pdf(values[i], mean[i], cov[i][i])
+
+    return prob
 
 
 data = generate_data(params)
